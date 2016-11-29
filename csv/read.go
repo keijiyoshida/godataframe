06750a51c6134bcde30cfb098adf5ac3ffa743c5
Keiji Yoshida <keijiyoshida.mail@gmail.com>
2016-11-30 02:07:03 +0900

    Update csv/read.go

--- csv/read.go
+++ csv/read.go
@@ -18,7 +18,7 @@ func Read(r io.Reader, config dataframe.Config) (*dataframe.DataFrame, error) {
 	return dataframe.New(data, config)
 }
 
-// ReadFile reads CSV data file, creates data frame data and returns it.
+// ReadFile reads a CSV data file, creates data frame data and returns it.
 func ReadFile(path string, config dataframe.Config) (*dataframe.DataFrame, error) {
 	f, err := os.Open(path)
 	if err != nil {
