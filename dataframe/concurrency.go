f0fb8afe5f0041287c820bd6509b7b042641082b
Keiji Yoshida <keijiyoshida.mail@gmail.com>
2016-12-03 14:10:13 +0900

    Add dataframe/init.go

--- dataframe/concurrency.go
+++ dataframe/concurrency.go
@@ -1,12 +1,13 @@
 package dataframe
 
-import "runtime"
+import "github.com/keijiyoshida/gostat/stat"
 
-var numConcurrency = runtime.GOMAXPROCS(0)
+var numConcurrency int
 
 // SetNumConcurrency sets the number of concurrency of the data frame processing.
 // The default value of this parameter is the maximum number of CPUs
 // that can be executing simultaneously.
 func SetNumConcurrency(n int) {
 	numConcurrency = n
+	stat.SetNumConcurrency(n)
 }
